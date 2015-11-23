9f60d6c2f1e9322850ea70583d51887d3fe548ba
jingleiZ <jingleizhang@nbp-35-57.nbp.ruw.rutgers.edu>
2015-11-23 15:52:48 -0500

    error fix
    sorry i did not notice the update from ernesto

--- www/js/controllers/OrdersCtrl.js
+++ www/js/controllers/OrdersCtrl.js
@@ -1,21 +1,8 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-angular.module('starter').controller('OrdersCtrl', function($scope, $stateParams, CurrentUser, Orders) {
-	Orders.get({user_id: CurrentUser.getUserId()
-	}, function(data){
-		$scope.orders = data.orders;
-	})
-=======
->>>>>>> Stashed changes
 angular.module('starter').controller('OrdersCtrl', function($scope, $stateParams, Orders, CurrentUser) {
     $scope.userId = CurrentUser.getUserId();
     
     Orders.get({user_id: $scope.userId}, function(data) {
         $scope.orders = data.orders;
     });
-<<<<<<< Updated upstream
-=======
->>>>>>> origin/master
->>>>>>> Stashed changes
+
 });
